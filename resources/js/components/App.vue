7e70817ae51c70c5154eadaacb528f3d518cc4cb
Gustavo <you@example.com>
2021-11-03 17:02:54 -0300

    ajuste de título e links da navbar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container">
         <div class="text-center" style="margin: 20px 0px 20px 0px;">
-            <span class="text-secondary">CRUD com Laravel 8 e Vue JS 3</span>
+            <span class="text-secondary title-aplicattion">CRUD com Laravel 8 e Vue JS 3</span>
         </div>
   
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse">
                 <div class="navbar-nav">
-                    <router-link to="/" class="nav-item nav-link">Tarefas</router-link>
+                    <router-link to="/" class="nav-item nav-link">Quadro de Tarefas</router-link>
                     <router-link to="/add" class="nav-item nav-link">Aicionar Tarefa</router-link>
                 </div>
             </div>
@@ -20,3 +20,22 @@
 <script>
     export default {}
 </script>
+
+
+<style scoped>
+.title-aplicattion {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.navbar-nav, .navbar-collapse{
+    display: flex;
+    justify-content: center;
+    font-weight: bold;
+}
+
+.nav-link{
+    font-weight: bold;
+    font-size: 1rem;
+}
+</style>
